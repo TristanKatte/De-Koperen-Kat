987dd86c10965fec25fb53677c65974a85d249c1
TristanKatte <tristankatte@hotmail.com>
2025-10-10 00:08:10 +0200

    feat(strapi): installed strapi within svelte project

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
 	import favicon from '$lib/assets/favicon.svg';
+	import Footer from '$lib/components/organisms/Footer.svelte';
+	import Header from '$lib/components/organisms/Header.svelte';
 
 	let { children } = $props();
 </script>
@@ -8,4 +10,15 @@
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
+<Header />
+
 {@render children?.()}
+<Footer />
+
+<style>
+	:global(body) {
+		margin: 0;
+		padding: 0;
+		box-sizing: border-box;
+	}
+</style>
