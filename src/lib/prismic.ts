91d41e3a598e57d884e4f6298dc902ccb0b80ad5
TristanKatte <tristankatte@hotmail.com>
2025-10-13 16:03:44 +0200

    fix: removed prismic from project

--- src/lib/prismic.ts
+++
@@ -1,8 +0,0 @@
-// src/lib/prismic.ts
-import * as prismic from "@prismicio/client";
-
-export const client = prismic.createClient(
-  import.meta.env.VITE_PRISMIC_REPO_URL, {
-    accessToken: import.meta.env.VITE_PRISMIC_API_TOKEN,
-  }
-);
--- src/routes/beers/+page.ts
+++
@@ -1,9 +0,0 @@
-import { createClient } from '$lib/prismicio';
-
-export async function load() {
-  const client = createClient();
-  const beers = await client.getAllByType('bieren', {
-    fetchLinks: [],
-  });
-  return { beers };
-}
