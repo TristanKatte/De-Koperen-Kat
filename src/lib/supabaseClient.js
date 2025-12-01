d1edaf8194fc73eaca85bdc53004605bfb5613fc
TristanKatte <tristankatte@hotmail.com>
2025-12-01 13:55:58 +0100

    fix: updated supabaseclient for ssr

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,9 +1,4 @@
 import { createClient } from "@supabase/supabase-js";
+import { SUPABASE_URL, SUPABASE_ANON_KEY } from "$env/static/private";
 
-import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY } from "$env/static/public"
-
-const supabaseUrl = PUBLIC_SUPABASE_URL;
-const supabaseKey = PUBLIC_SUPABASE_ANON_KEY;
-
-export const supabase = createClient(supabaseUrl, supabaseKey);
-        
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
