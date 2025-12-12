2eade40c382d1cb869e8254863b18058fe148d96
TristanKatte <tristankatte@hotmail.com>
2025-12-12 16:18:54 +0100

    feat: added page setup

---
+++ src/routes/tasting-room/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+	import BookingForm from "$lib/components/organisms/BookingForm.svelte";
+	import TastingRoomSection from "$lib/components/organisms/TastingRoomSection.svelte";
+
+</script>
+
+<svelte:head>
+    <title>Proeflokaal</title>
+</svelte:head>
+
+<section class="tasting-room-section">
+<TastingRoomSection />
+</section>
